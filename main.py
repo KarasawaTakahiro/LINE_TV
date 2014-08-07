829bfbbcbef96120e0931ca09f2b6e4c96a37eb5
tKarasawa <tkhr.karasawa@gmail.com>
2014-08-08 02:09:09 +0900

    主なページは完成

--- main.py
+++ main.py
@@ -3,6 +3,40 @@
 
 # main file
 
-from bottle import Bottle, route
+from bottle import run, route, post, get, request, redirect
 
-#dddddddddddd
+# name属性
+N_TEXT = "text"
+N_SEND_BUTTON = "send"
+
+# Page URL
+P_INDEX = "index"
+P_ADD = "add"
+
+# html
+H_INDEX = ""
+
+@route("/%s"%P_INDEX)
+def index():
+    return '''
+    <p>TopPage</p>
+    %s
+    <br />
+    <form action="/%s" method="post">
+        <input name="%s" type="text" />
+        <input type="submit" value="送る">
+    </form>
+    ''' % ("データベースを参照してトーク履歴からHTMLを作って表示", P_ADD, N_TEXT)
+
+@post("/%s" % P_ADD)
+def do_send():
+    text = request.forms.get(N_TEXT)
+    print "get: " + text
+
+    # データベースに登録する
+
+    redirect("/%s"%P_INDEX)
+
+
+run(host="localhost", port=8080)
+
